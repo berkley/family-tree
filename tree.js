951c4f578a363ece65bc345cfd47b58205f8f463
Chad Berkley <cberkley@flightstats.com>
2018-01-03 22:14:37 -0800

    adding data transformation...not quite working yet

--- tree.js
+++ tree.js
@@ -1,10 +1,36 @@
 var csv = require("fast-csv");
- 
+var family = {};
+//cols: [ 'Name', 'DOB', 'DOD', 'Mother', 'Father', 'Spouse1', 'Spouse2' ]
+
 csv
  .fromPath("family-data.csv")
  .on("data", function(data){
-     console.log(data);
+     // console.log(data);
+     var f = family[data[0]];
+     if(f) {
+     	console.log("ERROR: person already exists");
+     } else {
+     	family[data[0]] = {"name":data[0], "children":[]};
+     	f = family[data[0]];
+     }
+
+     //check if f has a mother or father and if they are in the hashtable already
+     if(data[3]) { //mother
+     	var m = family[data[3]];
+     	if(m) {
+     		family[data[3]].children.push(f);
+     	}
+     } 
+
+     if(data[4]) { //father
+     	var d = family[data[4]];
+     	if(d) {
+     		family[data[4]].children.push(f);
+     	}
+     }
+
  })
  .on("end", function(){
+ 	console.log("family:", JSON.stringify(family, null, 4));
      console.log("done");
  });
