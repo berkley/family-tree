a1d5fad8f998ad4f237fd12b391684b91f517a6e
Chad Berkley <cberkley@flightstats.com>
2018-01-09 21:51:44 -0800

    working on script to tranform csv to tree obj

--- tree.js
+++ tree.js
@@ -3,34 +3,28 @@ var family = {};
 //cols: [ 'Name', 'DOB', 'DOD', 'Mother', 'Father', 'Spouse1', 'Spouse2' ]
 
 csv
- .fromPath("family-data.csv")
+ .fromPath("family-data-test.csv")
  .on("data", function(data){
      // console.log(data);
      var f = family[data[0]];
      if(f) {
      	console.log("ERROR: person already exists");
      } else {
-     	family[data[0]] = {"name":data[0], "children":[]};
+     	family[data[0]] = {"name":data[0], "DOB":data[1], "DOD":data[2], "mother":data[3], "father":data[4], "spouse1":data[5], "spouse2":data[6], "children":[]};
      	f = family[data[0]];
      }
-
-     //check if f has a mother or father and if they are in the hashtable already
-     if(data[3]) { //mother
-     	var m = family[data[3]];
-     	if(m) {
-     		family[data[3]].children.push(f);
-     	}
-     } 
-
-     if(data[4]) { //father
-     	var d = family[data[4]];
-     	if(d) {
-     		family[data[4]].children.push(f);
-     	}
-     }
-
  })
  .on("end", function(){
  	console.log("family:", JSON.stringify(family, null, 4));
-     console.log("done");
+     
+     Object.keys(family).forEach(function(name) {
+       var name = o[key];
+       var tree = buildTree(family);
+     });
+
+     console.log("tree:", JSON.stringify(tree, null, 4));
  });
+
+ function buildTree(hash) {
+     
+ };
